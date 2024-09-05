1d11e822472d24a0602a56f386db230c18c7f9e7
GrimmerDJ <dgrimmer.exe@gmail.com>
2024-09-05 21:56:01 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Define the phrase to repeat
+        String wTj = "Welcome to Java.";
+
+        // Use a for loop to repeat the phrase 5 times
+        for (int i = 0; i < 5; i++) {
+            System.out.println(wTj);
+        }
+    }
+}
